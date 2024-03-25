5887ddb073a0e4707eb880f198f394f3b58eba8a
Niko <nikostephen@gmail.com>
2024-03-24 21:48:49 -0700

    tracked user choice selections

--- script.js
+++ script.js
@@ -2,13 +2,29 @@ let stats = {
     wins: 0,
     losses: 0,
     ties: 0,
+    rock: 0,
+    paper: 0,
+    scissors: 0,
 }
+
 let playAgain = true;
 
 while (playAgain) {
     let choices = ["Rock", "Paper", "Scissors"];
 
     let userChoice = prompt("Rock, Paper, or Scissors?");
+    if (userChoice === "Rock") {
+        stats.rock ++;
+    }
+
+    else if (userChoice === "Paper") {
+        stats.paper ++;
+    }
+
+    else if (userChoice === "Scissors") {
+        stats.scissors ++;
+    }
+    
     console.log(userChoice);
 
     let rngChoice = Math.floor(Math.random() * 3);
@@ -25,39 +41,39 @@ while (playAgain) {
     }
     // comparison between user and computer
     // rock beats scissors
-    if (userChoice === "Rock" && cpuChoice === "Scissors") {
+    else if (userChoice === "Rock" && cpuChoice === "Scissors") {
         window.alert("You Win!");
-        stats.wins = stats.wins + 1;
+        stats.wins ++;
     }
 
     // scissors beats paper
     else if (userChoice === "Scissors" && cpuChoice === "Paper") {
         window.alert("You Win!");
-        stats.wins = stats.wins + 1;
+        stats.wins ++;
     }
 
     // paper beats rock
     else if (userChoice === "Paper" && cpuChoice === "Rock") {
         window.alert("You Win!");
-        stats.wins = stats.wins + 1;
+        stats.wins ++;
     }
 
     // if two of the same variables are chosen = draw
     else if (userChoice === cpuChoice) {
         window.alert("Draw!");
-        stats.ties = stats.ties + 1;
+        stats.ties ++;
     }
     // all losses
     else {
         window.alert("You Lose...")
-        stats.losses = stats.losses + 1;
+        stats.losses ++;
     }
 
     console.log(stats);
     playAgain = confirm("Would you like to play again?");
 }
 
-window.alert(`Here are your stats!\nWins: ${stats.wins}\nTies: ${stats.ties}\nLosses: ${stats.losses}`);
+window.alert(`Here are your stats!\nWins: ${stats.wins}\nTies: ${stats.ties}\nLosses: ${stats.losses}\nRock: ${stats.rock}\nPaper: ${stats.paper}\nScissors: ${stats.scissors}`);
 
 
 // choice is confirmed
